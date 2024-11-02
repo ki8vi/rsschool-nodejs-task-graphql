23abc859dd6f4a743a20b7c0aeb0d1a344757ee2
ki8vi <oneofbln@gmail.com>
2024-11-02 14:44:51 +0500

    fix: types of mutations

--- src/routes/graphql/mutations.ts
+++ src/routes/graphql/mutations.ts
@@ -13,55 +13,68 @@ type CreateUserArg = {
     }
 }
 
+interface CreateProfileArg {
+  dto: {
+      isMale: boolean;
+      yearOfBirth: number;
+      memberTypeId: string;
+      userId: string;
+  };
+}
+
+interface CreatePostArg {
+  dto: {
+      title: string;
+      content: string,
+      authorId: string,
+  };
+}
+
+type Subs = { userId: string, authorId: string };
+
 const mutation = new GraphQLObjectType({
     name: 'Mutations',
     fields: {
       createUser: {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        type: User,
+      
+        type:  new GraphQLNonNull(User),
         args: { dto: { type: new GraphQLNonNull(CreateUserInput) } },
         resolve: async (__, { dto }: CreateUserArg, ctx: Ctx) => {
             return ctx.prisma.user.create({ data: dto });
         },
       },
       createProfile: {
-        type: Profile,
+        type: new GraphQLNonNull(Profile),
         args: { dto: { type: new GraphQLNonNull(CreateProfileInput) } },
-        resolve: async (__, { dto }, context: Ctx) => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        resolve: async (__, { dto }: CreateProfileArg, context: Ctx) => {
           return context.prisma.profile.create({ data: dto });
         },
       },
       createPost: {
         type: Post,
         args: { dto: { type: new GraphQLNonNull(CreatePostInput) } },
-        resolve: async (__, { dto }, context: Ctx) => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        resolve: async (__, { dto }: CreatePostArg, context: Ctx) => {
           return context.prisma.post.create({ data: dto });
         },
       },
       changePost: {
         type: Post,
         args: { id: { type: new GraphQLNonNull(UUIDType) }, dto: { type: new GraphQLNonNull(ChangePostInput) } },
-        resolve: async (__, { id, dto }, context: Ctx) => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        resolve: async (__, { id, dto }: { id: string, dto: { title: string, content: string } }, context: Ctx) => {
           return context.prisma.post.update({ where: { id }, data: dto });
         },
       },
       changeProfile: {
         type: Profile,
         args: { id: { type: new GraphQLNonNull(UUIDType) }, dto: { type: new GraphQLNonNull(ChangeProfileInput) } },
-        resolve: async (__, { id, dto }, context: Ctx) => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        resolve: async (__, { id, dto }: { id: string, dto: { isMale: boolean, yearOfBirth: number } }, context: Ctx) => {
           return context.prisma.profile.update({ where: { id }, data: dto });
         },
       },
       changeUser: {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        type: User,
+        type: new GraphQLNonNull(User),
         args: { id: { type: new GraphQLNonNull(UUIDType) }, dto: { type: new GraphQLNonNull(ChangeUserInput) } },
-        resolve: async (__, { id, dto }, context: Ctx) => {
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        resolve: async (__, { id, dto }: { id: string, dto: { name: string, balance: number } }, context: Ctx) => {
           return context.prisma.user.update({ where: { id }, data: dto });
         },
       },
@@ -95,14 +108,9 @@ const mutation = new GraphQLObjectType({
           userId: { type: new GraphQLNonNull(UUIDType) },
           authorId: { type: new GraphQLNonNull(UUIDType) },
         },
-        resolve: async (__, { userId, authorId }, context: Ctx) => {
+        resolve: async (__, { userId, authorId }: Subs, context: Ctx) => {
           await context.prisma.subscribersOnAuthors.create({
-            data: {
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-              subscriberId: userId,
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-              authorId: authorId,
-            },
+            data: { subscriberId: userId, authorId: authorId },
           });
           return 'plus subs )';
         },
@@ -113,19 +121,14 @@ const mutation = new GraphQLObjectType({
           userId: { type: new GraphQLNonNull(UUIDType) },
           authorId: { type: new GraphQLNonNull(UUIDType) },
         },
-        resolve: async (__, { userId, authorId }, context: Ctx) => {
+        resolve: async (__, { userId, authorId }: Subs, context: Ctx) => {
           await context.prisma.subscribersOnAuthors.deleteMany({
-            where: {
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-              subscriberId: userId,
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-              authorId: authorId,
-            },
+            where: { subscriberId: userId, authorId: authorId },
           });
           return 'just bye';
         },
       },
     },
-  });
+});
 
 export default mutation;
